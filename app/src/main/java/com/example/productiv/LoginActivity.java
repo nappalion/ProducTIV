1d4e09f934471ef819ad7d8ec3f5314f9474525c
nipplelion <piolotorrico@yahoo.com>
2021-12-24 12:27:56 -0800

    Add LoginActivity

---
+++ app/src/main/java/com/example/productiv/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.productiv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername;
+    EditText etPassword;
+    CheckBox btnRemember;
+    Button btnLogin;
+    Button btnForgot;
+    Button btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRemember = findViewById(R.id.btnRemember);
+        btnForgot = findViewById(R.id.btnForgot);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
