7286b33a0f291a686dedc531c255dd686adc676d
nipplelion <piolotorrico@yahoo.com>
2021-12-24 12:55:28 -0800

    Add Login and Signup UI and attach views

--- app/src/main/java/com/example/productiv/SignupActivity.java
+++ app/src/main/java/com/example/productiv/SignupActivity.java
@@ -3,12 +3,31 @@ package com.example.productiv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignupActivity extends AppCompatActivity {
+
+    EditText etEmail;
+    EditText etUsername;
+    EditText etPassword;
+    EditText etVerifyPassword;
+    Button btnSignup;
+    Button btnLogin;
+    ImageView ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        etEmail = findViewById(R.id.etEmail);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etVerifyPassword = findViewById(R.id.etVerifyPassword);
+        btnSignup = findViewById(R.id.btnSignup);
+        btnLogin = findViewById(R.id.btnLogin);
+        ivBack = findViewById(R.id.ivBack);
     }
 }
