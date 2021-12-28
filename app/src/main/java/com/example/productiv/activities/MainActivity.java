6b8e0305089b6f79720cc50364275d585f7ee544
nipplelion <piolotorrico@yahoo.com>
2021-12-27 22:40:07 -0800

    Add Timer Fragment UI

--- app/src/main/java/com/example/productiv/activities/MainActivity.java
+++ app/src/main/java/com/example/productiv/activities/MainActivity.java
@@ -31,8 +31,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         // Initialize views
         ivSettings = findViewById(R.id.ivSettings);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
