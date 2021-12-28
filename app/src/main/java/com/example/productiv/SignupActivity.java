a5d1bb1e69de9c9878e52f577a9dd93ec0cf7e5a
nipplelion <piolotorrico@yahoo.com>
2021-12-27 16:50:39 -0800

    Add Firebase signup and authentication

--- app/src/main/java/com/example/productiv/SignupActivity.java
+++ app/src/main/java/com/example/productiv/SignupActivity.java
@@ -1,13 +1,24 @@
 package com.example.productiv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -18,6 +29,8 @@ public class SignupActivity extends AppCompatActivity {
     Button btnSignup;
     Button btnLogin;
     ImageView ivBack;
+    private FirebaseAuth mAuth;
+    public static final String TAG = "SignupActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +45,25 @@ public class SignupActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         ivBack = findViewById(R.id.ivBack);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         etUsername.setText(getIntent().getStringExtra("username"));
+
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isValid(etEmail.getText().toString())
+                        && etPassword.getText().toString().equals(etVerifyPassword.getText().toString())) {
+                    //  Create account
+                    createAccount();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,9 +80,47 @@ public class SignupActivity extends AppCompatActivity {
         });
     }
 
+    public void createAccount() {
+        mAuth.createUserWithEmailAndPassword(etEmail.getText().toString(), etPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goMainActivity();
+                            // updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            // updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    // Check if email is valid using Matcher and Pattern
+    private boolean isValid(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null) return false;
+        return pat.matcher(email).matches();
+    }
+
     public void goLoginActivity() {
         Intent i = new Intent(SignupActivity.this, LoginActivity.class);
         i.putExtra("username", etUsername.getText().toString());
+        startActivity(i);
+    }
+
+    public void goMainActivity() {
+        Intent i = new Intent(SignupActivity.this, MainActivity.class);
         startActivity(i);
     }
 }
