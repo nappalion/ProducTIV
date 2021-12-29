2ac2f700e104e3f971f01ed600f93044920bd98f
nipplelion <piolotorrico@yahoo.com>
2021-12-29 15:01:31 -0800

    Add CountDownTimer

--- app/src/main/java/com/example/productiv/fragments/TimerFragment.java
+++ app/src/main/java/com/example/productiv/fragments/TimerFragment.java
@@ -4,13 +4,30 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.example.productiv.R;
 
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 public class TimerFragment extends Fragment {
+
+    private TextView tvTimer;
+    private ToggleButton btnPlay;
+    public static final String TAG = "TimerFragment";
+    private CountDownTimer countDownTimer;
+
+    // Initialize timer duration
+    long startTime = TimeUnit.MINUTES.toMillis(1);
 
     public TimerFragment() {
         // Required empty public constructor
@@ -19,6 +36,49 @@ public class TimerFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Initialize countdown timer
+        countDownTimer = new CountDownTimer(startTime, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // When tick
+                // Convert millisecond to minute and second
+                int seconds = (int) (millisUntilFinished / 1000);
+                int minutes = seconds / 60;
+                seconds = seconds % 60;
+
+                String timerDuration = String.format("%02d:%02d", minutes, seconds);
+                Log.i(TAG, timerDuration);
+                // Set converted string on text view
+                tvTimer.setText(timerDuration);
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(getContext(), "Countdown timer has ended", Toast.LENGTH_SHORT).show();
+            }
+        }.start();
+    }
+
+    //
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvTimer = getView().findViewById(R.id.tvTimer);
+        btnPlay = getView().findViewById(R.id.btnPlay);
+
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnPlay.isChecked()) {
+
+                }
+                else {
+
+                }
+            }
+        });
     }
 
     @Override
